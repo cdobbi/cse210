f0b71e563597d951522f5ff970486daa72b57adc
cdobbi <dob24001@byui.edu>
2024-08-05 08:41:01 -0600

    ...

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Prep3
+namespace Prep3_GuessingMagicNumber
 {
     class Program
     {
@@ -8,12 +8,13 @@ namespace Prep3
         {
             Random Generator = new Random();
             string keep_playing = "yes";
+
             while (keep_playing == "yes")
 
             {
                 int magic_number = Generator.Next(1, 11);
                 int guess = 0;
-                int guess_count = 0; 
+                int guess_count = 0;
 
                 while (guess != magic_number)
                 {
@@ -25,15 +26,13 @@ namespace Prep3
                     {
                         Console.WriteLine("Too low!");
                     }
+
                     else if (guess > magic_number)
                     {
                         Console.WriteLine("Too high!");
                     }
-                                            
                 }
-
                 Console.Write($"You guessed it. It took you {guess_count} guesses. Would you like to play again? (yes/no) ");
-
                 keep_playing = Console.ReadLine();
 
                 if (keep_playing != "yes")
