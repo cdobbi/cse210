91642cd2b682615efe81b7440213be5273e8b560
cdobbi <dob24001@byui.edu>
2024-08-07 10:48:32 -0600

    completed prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -8,6 +8,43 @@ namespace Prep5
         static void Main(string[] args)
         {
             Console.WriteLine("This is Prep 5");
+            Console.WriteLine();
+            DisplayMessage();
+            string name = PromptUserName();
+            int number = PromptFavoriteNumber();
+            int squared = NumberSquared(number);
+            DisplayResult(name, squared);
+            Console.WriteLine();
+        }
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Enter your first and last name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+
+            return name;
+        }
+        static int PromptFavoriteNumber()
+        {
+            Console.Write("Enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+        static int NumberSquared(int number)
+        {
+            int square = number * number;
+
+            return square;
+        }
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your favorite number is {square}.");
         }
     }
 }
+
