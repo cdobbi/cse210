864db738fb668d6ebb24255fcccaa17424267d1a
cdobbi <dob24001@byui.edu>
2024-08-27 13:10:34 -0600

    Prep 2 core requirements completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,35 @@ using System;
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade as a percentage? ");
+        string userInput = Console.ReadLine();
+        int percent = int.Parse(userInput);
+
+        
+        
+        if (percent >= 90)
+        {
+            Console.WriteLine("You earned an: A");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("You earned a: B");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("You earned a: C");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("You earned a: D");
+        }
+        else
+        {
+            Console.WriteLine("You earned a: F");
+            Console.WriteLine("Keep trying!");
+
+        }
     }
 }
