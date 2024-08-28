5f83110406a6f763539945b33b34656c6aeb1569
cdobbi <dob24001@byui.edu>
2024-08-28 10:06:12 -0600

    Prep 2 Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,35 +3,66 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    
+
     {
         Console.Write("What is your grade as a percentage? ");
         string userInput = Console.ReadLine();
-        int percent = int.Parse(userInput);
+        int grade = int.Parse(userInput);
+        string sign = "";
+        int lastDigit = grade % 10;
 
-        
-        
-        if (percent >= 90)
+        if (grade >= 93)
         {
-            Console.WriteLine("You earned an: A");
+            sign = " ";
+            Console.WriteLine($"You earned an: A{sign}");
         }
-        else if (percent >= 80)
+        else if (grade >= 90)
         {
-            Console.WriteLine("You earned a: B");
+            if (lastDigit >= 7)
+                sign = "+";
+            else if (lastDigit < 3)
+                sign = "-";
+            else
+                sign = " ";
+            Console.WriteLine($"You earned an: A{sign}");
         }
-        else if (percent >= 70)
+        else if (grade >= 80)
         {
-            Console.WriteLine("You earned a: C");
+            if (lastDigit >= 7)
+                sign = "+";
+            else if (lastDigit < 3)
+                sign = "-";
+            else
+                sign = " ";
+
+            Console.WriteLine($"You earned a: B{sign}");
         }
-        else if (percent >= 60)
+        else if (grade >= 70)
         {
-            Console.WriteLine("You earned a: D");
+            if (lastDigit >= 7)
+                sign = "+";
+            else if (lastDigit < 3)
+                sign = "-";
+            else
+                sign = " ";
+            Console.WriteLine($"You earned a: C{sign}");
+        }
+        else if (grade >= 60)
+        {
+            if (lastDigit >= 7)
+                sign = "+";
+            else if (lastDigit < 3)
+                sign = "-";
+            else
+                sign = " ";
+            Console.WriteLine($"You earned a: D{sign}");
         }
         else
         {
-            Console.WriteLine("You earned a: F");
+            if (grade <= 59)
+                sign = " ";
+            Console.WriteLine($"You earned a: F{sign}");
             Console.WriteLine("Keep trying!");
-
         }
     }
 }
