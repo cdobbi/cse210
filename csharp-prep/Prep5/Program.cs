6703b13d5c8d71a3dbb443e43ece1d8b6e90e6f6
cdobbi <dob24001@byui.edu>
2024-08-29 12:27:16 -0600

    Prep 5 Completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Prep5");
+        Console.WriteLine("");
+        // returnType FunctionName(dataType parameter1, dataType parameter2)
+        DisplayWelcomeMessage();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquaredNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+        Console.WriteLine("");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("");
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+
+        return number;
+    }
+
+    static int SquaredNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
+        Console.WriteLine("");
     }
 }
+
+// PromptUserName - Asks for and returns the user's name (as a string)
+// PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+// SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+// DisplayResult - Accepts the user's name and the squared number and displays them.
+// Your Main function should then call each of these functions saving the return values and passing data to them as necessary.
+
+// Welcome to the program!
+// Please enter your name: Brother Burton
+// Please enter your favorite number: 42
+// Brother Burton, the square of your number is 1764
