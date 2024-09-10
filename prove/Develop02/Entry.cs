38d599f1f2843128124fcea9159b9bed0fb64746
Christi Dobbins <christina.dobbina@gmail.com>
2024-09-10 15:14:59 -0600

    Journal practice

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,11 +1,21 @@
 public class Entry
 {
     public string _date;
-    public string _promptText;
     public string _entryText;
+    public string _randomTopic;
 
+    public Entry(string date, string entryText, string randomTopic)
+    {
+        _date = date;
+        _entryText = entryText;
+        _randomTopic = randomTopic;
+    }
     public void Display()
     {
-
+        Console.WriteLine();
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Topic: {_randomTopic}");
+        Console.WriteLine($"Entry: {_entryText}");
+        Console.WriteLine();
     }
 }
