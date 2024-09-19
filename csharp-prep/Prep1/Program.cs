9005d16dac31caa3542c68dfe06d67a379904405
Christi Dobbins <christina.dobbina@gmail.com>
2024-09-19 06:26:29 -0600

    9/19 Prep1 Fixed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,7 +8,8 @@ class Program
         string first = Console.ReadLine();
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
-        Console.WriteLine($"Your name is {last}, {first} {last}");
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
 
