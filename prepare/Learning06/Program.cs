fa11657ef4a4689129685e02d25a219b97700b37
cdobbi <dob24001@byui.edu>
2024-10-21 16:06:04 -0600

    ...

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -27,3 +27,291 @@ class Program
     }
 }
 
+CSE 210: Programming with Classes
+BYU-I logo
+Home
+W1
+W2
+W3
+W4
+W5
+W6
+W7
+Polymorphism Learning Activity
+Overview
+In this activity you will learn and practice the principle of Polymorphism.
+
+Prepare
+What is Polymorphism?
+Polymorphism is the ability to take on many forms. In programming, this principle is shown when one line of code can have different behavior depending on the context.
+
+Method Overriding
+To see polymorphism in action with objects and inheritance, you first need to learn about method overriding. Method overriding is the ability of a child class to override, or change the behavior of, a method that it inherited from a parent class. The method name stays the same but the behavior, or the code to run is different.
+
+An Example
+When using inheritance, a derived class can inherit both member variables and methods from a super-class. For example, a payroll system might define an employee that had a name, tax id number, address, and many other attributes. It may also have a method to calculate the pay for that employee.
+
+For a salary employee, perhaps the salary is just returned as shown in this example:
+
+
+// a parent class
+public class Employee
+{
+  private float salary = 100f;
+
+  public float CalculatePay()
+  {
+    return salary;
+  }
+}
+But if the payroll system also has to account for hourly employees that get paid a certain amount of money based on the number of hours they worked. These employees are very similar to the standard employee class, but they need different logic for the CalculatePay method. This could be defined in a child class that overrides the method from the Employee class. To do this, we first mark the method in the base class with the keyword virtual which tells C# that this method is eligible for another class to override it. Then, in the child class, we use the keyword override as shown in this example:
+
+
+// the parent class showing the "virtual" keyword included
+public class Employee
+{
+  private float salary = 100f;
+
+  public virtual float CalculatePay()
+  {
+    return salary;
+  }
+}
+
+// a child class
+public class HourlyEmployee : Employee
+{
+  private float rate = 9f;
+  private float hours = 100f;
+
+  public override float CalculatePay()
+  {
+    return rate * hours; // pay is calculated differently
+  }
+}
+
+
+// Changing a behavior this way is called method overriding. Different languages have slightly different syntax for overriding methods. In C# you override the method using the keywords virtual and override in the parent and child class methods as shown.
+
+// Notice that both Employees and HourlyEmployees have a method for CalculatePay, so in that way they are the same. But the actual behavior or code for the method is different.
+
+// A Powerful Combination
+// Inheritance and Method Overriding are a powerful combination. Used together they provide a way to vary runtime behavior according to the context.
+
+// Recall from the lesson on Inheritance that you should be able to use a derived object anywhere you can use the base class (the Liskov Substitution Principle). With this in mind, if you create a list of Employee objects, you should be able to insert HourlyEmployee objects into the list as well.
+
+// Review the following code carefully.
+
+
+// // Create a list of Employees
+// List<Employee> employees = new List<Employee>();
+
+// // Create different kinds of employees and add them to the same list
+// employees.add(new Employee());
+// employees.add(new HourlyEmployee());
+
+// // Get a custom calculation for each one
+// foreach(Employee employee in employees)
+// {
+//   float pay = employee.CalculatePay();
+//   Console.WriteLine(pay);
+// }
+// Output:
+
+
+// 100
+// 900
+// In this example, a new instance of Employee and HourlyEmployee are added to the employees list. In the loop that follows, the CalculatePay method is invoked for each one. The actual method that is called and the resulting value depends on the context, or the type of employee, during each iteration. If the object happens to be an Employee the base method will be called. However, if the object is an HourlyEmployee the version defined for hourly employees will be used instead.
+
+// The significance of the last statement cannot be overstated. All it takes to vary the behavior of the loop is to create new derivations of Employee, override the CalculatePay method, and add an instance to the list. None of the previously written code needs to be modified in any way. Changing the program is easy!
+
+// Polymorphism in Action
+// The previous code example shows polymorphism in action. Recall the following line of code from that example:
+
+
+// float pay = employee.CalculatePay();
+// As stated, this exact same line of code can take on "many forms", or more specifically, it will call different methods depending on the type of the employee object encountered at run time.
+
+// Another Example: Parameter Passing
+// In addition to seeing polymorphism used in the context of iterating through a list of base class objects. You can see it in action by passing an object to a method. Consider the following code:
+
+
+// public class Program
+// {
+//     // ...
+
+//     static void DisplayPayCheck(Employee e)
+//     {
+//         float pay = e.CalculatePay();
+//         // ...
+//     }
+// }
+// Notice that in this example, the DisplayPayCheck function has a parameter of Employee. Again, we should be able to substitute any derived class and have it work, so you could call the function with an HourlyEmployee object, and it will work just fine. The code e.CalculatePay() will call the correct method based on the actual object at run time.
+
+// Another Example: Return Values
+// Another common use of Polymorphism is that when the return type of a method is a base class, you can also return objects of derived classes. Consider the following code:
+
+
+// public class Program
+// {
+//     // ...
+
+//     static Employee GetManager()
+//     {
+//         // ... code here to find the manager ...
+//         return theManager;
+//     }
+
+//     static void DisplayManagerPay()
+//     {
+//         Employee manager = GetManager();
+//         float pay = manager.CalculatePay();
+//         // ...
+//     }
+// }
+// The code that determines the manager may return may end up returning either a base class Employee object or it may return an HourlyEmployee object. Regardless of the type of employee returned, the manager.CalculatePay() code will call the appropriate method.
+
+// Abstract Methods
+// In the example above, the base class contained a default implementation for the CalculatePay method that worked for base employees. But sometimes, it is not possible to create a good default method. For example, instead of having the base class represent a Salary Employee and then the derived class is for the Hourly Employee, you might define the base class for a generic Employee with two derived classes. In this case, you couldn't provide a good default implementation of the method in the base class, so you would want to leave it blank.
+
+// A blank virtual method has a special name, it is called an Abstract method. Any class that has at least one abstract method is an Abstract class. This means that the base abstract class cannot be directly instantiated, you can only create objects from the derived types.
+
+// You specify abstract methods with the abstract keyword instead of virtual. Then, the class definition must also contain the abstract keyword. For example:
+
+
+// // the parent class showing the "virtual" keyword included
+// public abstract class Employee
+// {
+//   private string _employeeName;
+
+//   // Notice the abstract method doesn't have a body at all (not even an empty one)
+//   // and it is followed by a semicolon.
+//   public abstract float CalculatePay();
+// }
+
+// // a child class
+// public class SalaryEmployee : Employee
+// {
+//   private float salary = 100f;
+
+//   public override float CalculatePay()
+//   {
+//     return salary;
+//   }
+// }
+
+// // a child class
+// public class HourlyEmployee : Employee
+// {
+//   private float rate = 9f;
+//   private float hours = 100f;
+
+//   public override float CalculatePay()
+//   {
+//     return rate * hours; // pay is calculated differently
+//   }
+// }
+// Its All About the Interface
+// The most important aspect of the previous example is the shared method called CalculatePay. It is a formal contract that all Employee derivations, no matter what their specific type, will provide the same capability, namely a method with the name CalculatePay and in this case, no parameters, and a return value of a float. That guarantee is relied on by any other parts of the program that use Employees of any kind.
+
+// Take a moment and cast your mind back to the second principle of programming with classes. One of the more important aspects of applying encapsulation was to focus on what a class must do rather than how it will do it. The same advice applies here.
+
+// Interfaces (optional)
+// Abstract methods give us a way to specify that a method must be present in derived classes without providing a default implementation. This idea is so powerful many times all we want to do is define the public methods that a derived class must have--we do not even want to provide any member variables or method bodies in the base class.
+
+// A base class that only contains these abstract methods and nothing else has a special name, it is called an Interface because it defines the interface or public methods that any classes that implement it must have. In this case, you define the "class" as an interface, and then, you don't have to specify that the methods are abstract or virtual or even public, because all of these things are implied. Consider the following code:
+
+
+// // the Employee interface
+// // The C# convention is that interface names begin with an I
+// public interface IEmployee
+// {
+//   float CalculatePay(); // interface method does not have a body
+// }
+
+// // a specific implementation of the Employee interface
+// public class SalariedEmployee : IEmployee
+// {
+//   private float salary = 100f;
+
+//   public float CalculatePay()
+//   {
+//     return salary;
+//   }
+// }
+
+// // another implementation of tbe Employee interface
+// public class HourlyEmployee : IEmployee
+// {
+//   private float rate = 9f;
+//   private float hours = 100f;
+
+//   public float CalculatePay()
+//   {
+//     return rate * hours;
+//   }
+// }
+// So which should you use, an Abstract class or an Interface? The answer depends on whether your base class will have any member variables or method bodies. If you want to provide those, then you should create an abstract class. If your base class is only there to define the methods that should be overridden, then you should use an Interface.
+
+// Video Example
+// Please watch the following example of how to use Polymorphism in C#.
+
+// Direct Link: Polymorphism in C# (17 minutes)
+
+
+// In Summary
+// Polymorphism is the fourth and crowning principle of programming with classes. Skillful use of abstraction, encapsulation and inheritance are all required to apply polymorphism effectively. The result is a simple but powerful mechanism for ensuring that programs are flexible and ready for change.
+
+// One of the recurring themes through all of this has been the importance of focusing on class contracts or the interface. Identifying, defining and developing them are of prime concern for those who practice programming with classes on a regular basis.
+
+// Activity Instructions
+// Practice the principle of polymorphism by writing a program that computes the areas of different shapes cut out of pieces of paper.
+
+// For all shapes, you need to keep track of the color of the paper and then have a method to compute the area. The area should not be stored as a member variable, but instead, you should store the length of the shapes sides and then compute the area as needed.
+
+// Your program should include squares (which store a color and a single side), rectangles (which store a color and two sides), and a circle (which store a color and a radius). You should create several kinds of shapes and put them into a single list. Then, iterate through the list and display their areas.
+
+// Design the Classes
+// Based on what you learned in inheritance, it seems reasonable to create a base shape class where you can include any responsibilities that all shapes have in common. Then you can create derived classes for the individual square, rectangle and circle shapes.
+
+// In this example all shapes have a color and a method to get the area, but the implementation of that method will be different for each kind of shape. Thus, the GetArea method should be declared in the base class, but you should override it in the derived classes.
+
+// These relationships can be seen with the following class diagram:
+
+// Class Diagram
+// Shape Class Diagram
+// Start the Project
+// Open the class project in VS Code.
+// Navigate to the Learning06 project in the prepare folder. Find the Program.cs file, which will be your entry point for the program.
+// Verify that you can run the project.
+// Create the base Shape class
+// In a new file, create the Shape class.
+// Add the color member variable and a getter and setter for it.
+// Create a constructor that accepts the color and set its.
+// Create a virtual method for GetArea().
+// Create the Square class
+// In a new file, create the Square class.
+// Make sure this class inherits from the base class.
+// Create a constructor that accepts the color and the side, and then call the base constructor with the color.
+// Create the _side attribute as a private member variable.
+// Override the GetArea() method from the base class and fill in the body of this function to return the area.
+// Test the Square class
+// Return to the Main method in Program.cs to test your code.
+// Create a Square instance, call the GetColor() and GetArea() methods and make sure they return the values you expect.
+// Create the Rectangle and Circle classes
+// Repeat the steps above for the Rectangle and Circle classes, putting them each in their own files, storing the necessary variables, and overriding the GetArea() for each.
+// Test these classes back in Main and make sure they work as expected.
+// Build a List
+// In your Main method, create a list to hold shapes (Hint: The data type should be List<Shape>).
+// Add a square, rectangle, and circle to this list.
+// Iterate through the list of shapes. For each one, call and display the GetColor() and GetArea() methods.
+// Sample Solution
+// When you have finished please compare your approach to the following sample solution (you may also use this sample solution as a guide if you need help finishing).
+
+// Learning Activity 06 Sample Solution.
+// Submission
+// Verify that each of your classes works as described above.
+// Commit and push your code to your GitHub repository.
+// Verify that you can see your updated code at GitHub.
+// Submit the Canvas quiz to report on your work.
+// Copyright © Brigham Young University-Idaho | All rights reserved
--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -7,3 +7,152 @@ class Program
         Console.WriteLine("Hello Develop06 World!");
     }
 }
+
+// CSE 210: Programming with Classes
+// BYU-I logo
+// Home
+// W1
+// W2
+// W3
+// W4
+// W5
+// W6
+// W7
+// Week 06 Develop: Eternal Quest Program
+// Problem Overview
+// Many people set goals for areas they want to improve in their lives. These goals can be very important to help us in our growth, such as highlighted in the Church's Children and Youth program. But despite our best intentions, we often struggle to follow through with these goals.
+
+// Think for a minute about what are the biggest challenges that keep someone from reaching the goals they have set?
+
+// There are many different reasons you might come up with that apply generally such as forgetting, not having enough time, and programs or applications could help with many of those. In addition to these general challenges, some that are specific to goals could include:
+
+// Many goals repeat and are never finished, such as studying the scriptures.
+// Many goals have to be completed a number of times before they can be finished, such as attending the temple 10 times.
+// While we recognize the value of our goals in the eternal sense, sometimes we need some shorter term "rewards" to help excite us along the way.
+// Solution Idea
+// Gamification is the idea of turning something into a game. It could include a number of different concepts, but often involves getting badges or awards, as you "level up" with more points.
+
+// Consider a program that keeps track of various kinds of goals that people have, which are goals in their real life, such as studying the scriptures each day, finishing a project, or even stopping a bad habit. This program could be set up to keep track of your progress on these goals and offer points, awards, or other celebrations to keep you encouraged to keep working.
+
+// A program like this could be more serious, and just keep track of goal progress, or it could be as funny and personal as you would want (for example, a level 13 Ninja Unicorn). The idea is to engage the part of our brain that loves seeing these "wins" and accomplishments as we work on our goals.
+
+// No matter what the exact format is, this program could help each of us on our "Eternal Quest" of returning to be with our Father in Heaven.
+
+// Specification
+// Write an Eternal Quest program to track various kinds of goals.
+
+// Your program can include whatever kinds of "gamification" components you like, but each program should have the following base functionality.
+
+// Functional requirements
+// Your program must do the following:
+
+// Provide for simple goals that can be marked complete and the user gains some value.
+// For example, if you run a marathon you get 1000 points.
+// Provide for eternal goals that are never complete, but each time the user records them, they gain some value.
+// For example, every time you read your scriptures you get 100 points.
+// Provide for a checklist goal that must be accomplished a certain number of times to be complete. Each time the user records this goal they gain some value, but when they achieve the desired amount, they get an extra bonus.
+// For example, if you set a goal to attend the temple 10 times, you might get 50 points each time you go, and then a bonus of 500 points on the 10th time.
+// Display the user's score.
+// Allow the user to create new goals of any type.
+// Allow the user to record an event (meaning they have accomplished a goal and should receive points).
+// Show a list of the goals. This list should show indicate whether the goal has been completed or not (for example [ ] compared to [X]), and for checklist goals it should show how many times the goal has been completed (for example Completed 2/5 times).
+// Allow the user's goals and their current score to be saved and loaded.
+// Design Requirements
+// In addition your program must:
+
+// Use inheritance by having a separate class for each kind of activity with a base class to contain any shared attributes or behaviors.
+// Use polymorphism by having derived classes override base class methods where appropriate.
+// Follow the principles of encapsulation and abstraction by having private member variables and putting related items in the same class.
+// Showing Creativity and Exceeding Requirements
+// Meeting the core requirements makes your program eligible to receive a 93%. To receive 100% on the assignment, you need to show creativity and exceed these requirements.
+
+// Here are some ideas you might consider:
+
+// Add your own ideas for gamification. This could include leveling up, earning certain bonuses, or other "fun" aspects to the quest.
+// Add additional kinds of goals, such as the ability to make progress towards a large goal (such as getting value for working towards running a marathon), or "negative goals" where they lose points for bad habits.
+// There are lots of ways to show creativity in this assignment. Pick anything that sounds fun to you!
+// Report on what you have done to exceed requirements by adding a description of it in a comment in the Program.cs file.
+
+// Video Demo
+// The following video demonstrates the way this program should work:
+
+// Direct link: Eternal Quest Demo (9 minutes)
+
+
+// Code Helps
+// You might find the following code helps useful in this project:
+
+// Reading Text Files in C#
+// The easiest way to read a text file in C# is to read the entire file into an array of strings (one per line) using the System.IO.File.ReadAllLines() function. Then, you can iterate through each string as you would with any list.
+
+// As you go through each string, you can split it based on a separator character and get the pieces you need by their index.
+
+// The following shows an example of reading a file and splitting up the strings.
+
+
+// string filename = "myFile.txt";
+// string[] lines = System.IO.File.ReadAllLines(filename);
+
+// foreach (string line in lines)
+// {
+//     string[] parts = line.Split(",");
+
+//     string firstName = parts[0];
+//     string lastName = parts[1];
+// }
+// There are many other ways to work with files in C# some of which would be more efficient if the file is very large and you don't want to store it all in memory at once, but this approach will work just fine for this project.
+
+// Writing Text Files in C#
+// You can create or write a text file in C# using a class the System.IO.StreamWriter class. When you create an object of this class you can use the Write() and WriteLine() methods in the same way as the Console.Write() methods, except that they will write the strings to the file instead of to the Console.
+
+// To make sure the file gets closed and cleaned up appropriately when you are done, it is best practice to put the StreamWriter object in a using() block. This works the same as a "with" block in Python and ensures that the resources are cleaned up when you leave that area of the code.
+
+// The following shows an example of writing text to a new file
+
+
+// // Don't forget to put this at the top, so C# knows where to find the StreamWriter class
+// using System.IO; 
+
+// ...
+
+// string fileName = "myFile.txt";
+
+// using (StreamWriter outputFile = new StreamWriter(filename))
+// {
+//     // You can add text to the file with the WriteLine method
+//     outputFile.WriteLine("This will be the first line in the file.");
+    
+//     // You can use the $ and include variables just like with Console.WriteLine
+//     string color = "Blue";
+//     outputFile.WriteLine($"My favorite color is {color}");
+// }
+// Saving and Loading objects in C#
+// Turning an object into a string so that you can save it, load it, or send it to another program on the internet, is called "Serializing" and "Deserializing" it. Depending on how complicated your object is, this can require you to be somewhat clever in how it is done. There are libraries of functions that others have written to help serialize and deserialize objects into various formats (for example, JSON, XML, etc.). While you are welcome to use one of these if you would like, they can often take a fair amount of work to configure correctly, so for our simple case, it is probably easier to not use one of them.
+
+// Instead, consider creating a method that returns a string containing the pieces of data that you need for your object, such as: GetStringRepresentation(). This might include the type of the object first (so you'll know later which one you should be using), and then any details of the object. For example, if you had a Person object, it might return a string like this: "Person:Brigham,Young".
+
+// Then, if you wanted to save a whole list of people, you could iterate through them and for each one, call the GetStringRepresentation() method and then save the string out to a file.
+
+// Similarly, when it is time to load the file, you could read in the line, and first split it based on the colon : to get the type of the object (Person) and the details ("Brigham,Young"). Then, you could create a method called CreatePerson that would accept the string of details ("Brigham,Young"). This method could split the string on the commas to get the various items. Then, it could create a new Person object, set all the values, and return it.
+
+// As a side note, this pattern of creating a method (or a class) to help create objects for you is common enough that it has a name: The Factory Pattern. In future courses, you will learn more about the Factory Pattern, and how to handle many of the complexities that could arise when creating objects.
+
+// Design
+// You will work with your team to create a design for this program. Then, you will each write the code for the program individually.
+
+// Final Design (Do not open until after your design meeting)
+// For reference purposes, here is a copy of the design that was created during the design activity.
+
+// In order to understand the decisions that led to this design, make sure to walk through the design activity step by step before using this design to start your code.
+
+// Eternal Quest program class diagram
+// Develop the Program
+// In the course repository, find the Develop06 project in the Prove folder and write your program there.
+
+// Submission
+// Develop the program using the principle of Polymorphism as described above.
+// Make sure to describe anything you have done to exceed the requirements in comments in the Program.cs file.
+// Commit your source code and push it to GitHub.
+// Verify that you can see your updated code at GitHub.
+// In Canvas, submit a link to your GitHub repository. In the submission comment, describe anything you have done to show creativity and exceed the core requirements.
+// Copyright © Brigham Young University-Idaho | All rights reserved
