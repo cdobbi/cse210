deb1fb9071e47595a2590c523b2cad2889cd4c15
cdobbi <dob24001@byui.edu>
2024-10-21 14:58:52 -0600

    Learning 06

---
+++ prepare/Learning06/Circle.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color couble radius) : base(color)
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
